ce60b883cc7e2a868807c3b3aa49963094c632c1
Anthony Bravo Berrio <anthony.bravo.berrio@gmail.com>
2020-04-06 15:09:58 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/components/Footer.styl';
 
 const Footer = () => (
   <div className="Footer">
-    <p className="Footer-title">Platzi Store - Anthony</p>
+    <p className="Footer-title">Platzi Store - Master</p>
     <p className="Footer-copy">Todos los Izquierdos Reservados</p>
   </div>
 );
